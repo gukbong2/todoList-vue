efd994d98c877d46e9390ab3e5761544b6d5c06e
gukbong2 <junyeon2459@gmail.com>
2019-02-01 22:45:32 +0900

    Modal 추가~~

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,63 +1,71 @@
 <template>
   <div class="inputBox shadow">
-    <input type="text" v-model="newTodoItem" placeholder="plz type what you have to do :)" v-on:keyup.enter="addTodo">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keypress.enter="addTodo">
     <span class="addContainer" v-on:click="addTodo">
-        <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고!</h3>
+      <span slot="footer" @click="showModal = false">할 일을 입력하세요!
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
   </div>
 </template>
 
-<script type="text/javascript">
-  export default {
-      data() {
-        return {
-          newTodoItem : ''
-        }
-      },
-      methods : {
-        addTodo() {
-          console.log(this.newTodoItem);
-
-          if(this.newTodoItem !== "") {
-            var value = this.newTodoItem && this.newTodoItem.trim();
-              this.$emit('addTodo', value);
-              this.clearInput();
-          }
-        },
-        clearInput() {
-          this.newTodoItem = '';
-        }
+<script>
+import Modal from './common/Modal.vue'
+export default {
+  data() {
+    return {
+      newTodoItem: '',
+      showModal: false
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+				this.$emit('addTodo', value)
+        this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  },
+  components: {
+    Modal: Modal
   }
+}
 </script>
 
 <style scoped>
-  input:focus {
-    outline : none;
-    /* 인풋 박스의 선 스타일 지정 */
-  }
-  .inputBox {
-    background : white;
-    height: 50px;
-    line-height: 50px; /*인풋 박스에 입력되는 텍스트의 중앙 정렬을 위해 설정 */
-    border-radius: 5px; /* 인풋 박스의 둥근 테두리~~ */
-  }
-  .inputBox input {
-    border-style: none;
-    font-size: 0.9em;
-  }
-  .addContainer {
-    float: right; /* 할일 추가 아이콘의 위치 정의 ~~*/
-    background : linear-gradient(to right, #6478FB, #8763FB);
-    display: block;
-    width : 3rem;
-    border-radius : 0 5px 5px 0;
-  }
-  .addBtn {
-    color : white;
-    vertical-align : middle; /*할 일 추가 아이콘의 수직 정렬 정의~~*/
-  }
-
-
-
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: inline-block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>
