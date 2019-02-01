3913fe5c7a2d13c3a92e2a7e5bf07892a94b6be1
gukbong2 <junyeon2459@gmail.com>
2019-02-01 22:16:54 +0900

    todo List!

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="plz type what you have to do :)" v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+        <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+      data() {
+        return {
+          newTodoItem : ''
+        }
+      },
+      methods : {
+        addTodo() {
+          console.log(this.newTodoItem);
+
+          if(this.newTodoItem !== "") {
+            var value = this.newTodoItem && this.newTodoItem.trim();
+              localStorage.setItem(value, value);
+              this.clearInput();
+          }
+        },
+        clearInput() {
+          this.newTodoItem = '';
+        }
+      }
+  }
+</script>
+
+<style>
+  input:focus {
+    outline : none;
+    /* 인풋 박스의 선 스타일 지정 */
+  }
+  .inputBox {
+    background : white;
+    height: 50px;
+    line-height: 50px; /*인풋 박스에 입력되는 텍스트의 중앙 정렬을 위해 설정 */
+    border-radius: 5px; /* 인풋 박스의 둥근 테두리~~ */
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9em;
+  }
+  .addContainer {
+    float: right; /* 할일 추가 아이콘의 위치 정의 ~~*/
+    background : linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width : 3rem;
+    border-radius : 0 5px 5px 0;
+  }
+  .addBtn {
+    color : white;
+    vertical-align : middle; /*할 일 추가 아이콘의 수직 정렬 정의~~*/
+  }
+
+
+
+</style>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,67 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" class="shadow" :key="todoItem">
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+            <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
+
+</template>
+
+
+<script type="text/javascript">
+  export default {
+    data() {
+      return {
+        todoItems : []
+      }
+    },
+    methods : {
+      removeTodo(todoItem, index) {
+        localStorage.removeItem(todoItem);
+        this.todoItems.splice(index, 1);
+        console.log(todoItem, index);
+      }
+    },
+    created() {
+      if (localStorage.length > 0) {
+        for (var i = 0; i < localStorage.length; i++) {
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none; /* 목록 아이템의 스타일을 지정*/
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+  li {
+    display: flex;/* 비율 기준의 레이아웃 방식인 flext로 지정*/
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding : 0 0.9rem;
+    background : white;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-left: 5px;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
+</style>
